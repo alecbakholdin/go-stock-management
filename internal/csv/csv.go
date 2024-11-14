3726b1829cadcca08c2f8709f58e2e8887c6dddc
Alec Bakholdin <abakho@icims.com>
2024-11-14 16:36:22 -0500

    csv parser works

---
+++ internal/csv/csv.go
@@ -0,0 +1,90 @@
+package csv
+
+import (
+	"cmp"
+	"encoding/csv"
+	"errors"
+	"io"
+	"reflect"
+	"slices"
+	"strconv"
+
+	"github.com/labstack/gommon/log"
+)
+
+func Parse[T any](r io.Reader, row *T) ([]T, error) {
+	rowType := reflect.TypeOf(*row)
+	if rowType.Kind() != reflect.Struct {
+		return nil, errors.New("row must be a struct")
+	}
+	reader := csv.NewReader(r)
+	header, err := reader.Read()
+	if err != nil {
+		return nil, errors.Join(errors.New("error reading header line"), err)
+	}
+	headerMap := GetHeaderMap(header, rowType)
+
+	lines, err := reader.ReadAll()
+	if err != nil {
+		return nil, errors.Join(errors.New("error reading csv body"), err)
+	}
+
+	output := []T{}
+	for _, line := range lines {
+		if len(line) != len(headerMap) {
+			log.Warn("line did not match expected length. Expected ", len(headerMap), " but found ", len(line), " ", line)
+			continue
+		}
+
+		outputStruct := new(T)
+		for i, f := range headerMap {
+			structValue := reflect.ValueOf(outputStruct).Elem().FieldByName(f.Name)
+			if !structValue.CanSet() {
+				log.Warn("cannot set unexported field " + structValue.Type().Name())
+				continue
+			}
+			switch structValue.Kind() {
+			case reflect.String:
+				structValue.SetString(line[i])
+			case reflect.Int:
+				if intVal, err := strconv.Atoi(line[i]); err != nil {
+					log.Warn("Invalid int value ", line[i], " on line ", i)
+				} else if structValue.OverflowInt(int64(intVal)) {
+					log.Warn("Int value overflow ", line[i], " on line ", i)
+				} else {
+					structValue.SetInt(int64(intVal))
+				}
+			case reflect.Float32:
+				fallthrough
+			case reflect.Float64:
+				if floatVal, err := strconv.ParseFloat(line[i], 64); err != nil {
+					log.Warn("Invalid float value ", line[i], " on line ", i)
+				} else if structValue.OverflowFloat(floatVal) {
+					log.Warn("Float value overflow ", line[i], " on line ", i)
+				} else {
+					structValue.SetFloat(floatVal)
+				}
+			default:
+				return nil, errors.New("Unsupported csv parse type " + structValue.Kind().String())
+
+			}
+		}
+		output = append(output, *outputStruct)
+	}
+
+	return output, nil
+}
+
+func GetHeaderMap(header []string, rowType reflect.Type) []*reflect.StructField {
+	headerMap := make([]*reflect.StructField, len(header))
+
+	for i := 0; i < rowType.NumField(); i++ {
+		field := rowType.Field(i)
+		csvTag := field.Tag.Get("csv")
+		idx := slices.Index(header, cmp.Or(csvTag, field.Name))
+		if idx >= 0 && headerMap[idx] == nil {
+			headerMap[idx] = &field
+		}
+	}
+	return headerMap
+}
---
+++ internal/csv/csv_test.go
@@ -0,0 +1,41 @@
+package csv
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHeaderMap(t *testing.T) {
+	type testStruct struct {
+		FirstField string `csv:"First"`
+		Second string
+		Third string `csv:"Eight"`
+	}
+
+	headerMap := GetHeaderMap([]string{"Second", "Third", "Eight", "First"}, reflect.TypeOf(testStruct{}))
+	assert.Equal(t, 4, len(headerMap))
+	assert.Equal(t, "Second", headerMap[0].Name)
+	assert.Nil(t, headerMap[1])
+	assert.Equal(t, "Third", headerMap[2].Name)
+	assert.Equal(t, "FirstField", headerMap[3].Name)
+}
+
+func TestParse(t *testing.T) {
+	type testStruct struct {
+		AnInt int	 `csv:"Int"`
+		AFloat float32 `csv:"Float"`
+		AString string `csv:"String"`
+	}
+	inputStr := `String,Float,Int
+string,1.01,3
+stringtwo,1.02,4
+`
+
+	outputArr, err := Parse(strings.NewReader(inputStr), &testStruct{})
+	if assert.NoError(t, err) {
+		assert.ElementsMatch(t, []testStruct{{3,1.01,"string"},{4,1.02,"stringtwo"}}, outputArr)
+	}
+}
